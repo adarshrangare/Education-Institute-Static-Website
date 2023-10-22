b6ad99482861b8b9941fa5ac86f88ecffceaf5fc
adarshrangare <akrangare@gmail.com>
2023-10-22 14:09:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+    // Menu Toggle
+
+const navLinks = document.querySelector(".nav-links");
+function showMenu(){
+    navLinks.style.right = 0;
+}
+
+function closeMenu(){
+    navLinks.style.right = "-40vw";
+}
